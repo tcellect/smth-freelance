403dc1170771ea0510c7afb7c38bb674b0d58f68
Deniss Orlov <aristarxus@Denisss-MacBook-Pro.local>
2021-10-01 17:38:57 +0300

    add endpoints, gorilla mux

--- index/main.go
+++ index/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// TODO
+// handle CRUD operations in orders
+// respond JSON
+func handleOrders(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func handleDashboard(w http.ResponseWriter, r *http.Request) {
+
+}
 
 func main() {
-	fmt.Println("hello freelance")
+	r := mux.NewRouter()
+	r.HandleFunc("/order/", handleOrders)
+	r.HandleFunc("/dashboard/", handleDashboard)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
