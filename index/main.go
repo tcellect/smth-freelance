9d71d6ca16aaa37b777950c5f1ae608f12842d08
Deniss Orlov <aristarxus@Denisss-MacBook-Pro.local>
2021-10-13 23:56:56 +0300

    add landing

--- index/main.go
+++ index/main.go
@@ -21,6 +21,7 @@ func handleDashboard(w http.ResponseWriter, r *http.Request) {
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/order/", handleOrders)
+	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("static"))))
 	r.HandleFunc("/dashboard/", handleDashboard)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
